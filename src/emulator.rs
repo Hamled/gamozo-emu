4347b3796f66e50b11212d184b56b5597fe41a2a
Charles Ellis <hamled@hamled.dev>
2020-07-18 05:06:53 +0700

    Rust JIT WIP

--- src/emulator.rs
+++ src/emulator.rs
@@ -1,12 +1,18 @@
 use crate::file::{File, FileSystem};
 use crate::mmu::{Mmu, Perm, Section, VirtAddr, PERM_EXEC};
 use crate::DEBUG;
+use std::cell::RefCell;
+use std::fmt;
+use std::fs;
+use std::io::Write;
 use std::path::Path;
 
 const DEBUG_PRINTS: bool = DEBUG > 0;
 const DEBUG_SYSCALL: bool = DEBUG > 1;
 const DEBUG_ALLOC_FAIL: bool = DEBUG > 2;
 const DEBUG_EXEC: bool = DEBUG > 5;
+
+const JIT_INSTS_MAX: usize = 1;
 
 /// All the state of the emulated system
 pub struct Emulator {
@@ -21,6 +27,9 @@ pub struct Emulator {
 
     // Files used by program (including stdin/out/err)
     filesystem: FileSystem,
+
+    jit_file: RefCell<Option<fs::File>>,
+    jit_insts: usize,
 }
 
 impl Emulator {
@@ -35,6 +44,9 @@ impl Emulator {
             break_max: VirtAddr(!0),
 
             filesystem: FileSystem::default(),
+
+            jit_file: RefCell::new(None),
+            jit_insts: 0,
         }
     }
 
@@ -49,6 +61,9 @@ impl Emulator {
             break_max: self.break_max,
 
             filesystem: self.filesystem.clone(),
+
+            jit_file: RefCell::new(None),
+            jit_insts: 0,
         }
     }
 
@@ -159,6 +174,9 @@ impl Emulator {
                 .read_perms(VirtAddr(pc as usize), Perm(PERM_EXEC))?;
 
             *instrs_execed += 1;
+
+            // JIT the instruction
+            self.jit_inst(pc, inst);
 
             // Execute the instruction if possible
             match self.exec_inst(pc, inst) {
@@ -726,6 +744,487 @@ impl Emulator {
 
         Ok(pc.wrapping_add(4))
     }
+
+    fn jit_inst(&mut self, pc: u64, inst: u32) {
+        let mut file = match *self.jit_file.borrow_mut() {
+            None => {
+                let mut file = fs::File::create("jitcode.rs").expect("Could not create jitcode.rs");
+                self.jit_file.replace(Some(file));
+                file
+            }
+            Some(file) => file,
+        };
+
+        if self.jit_insts == 0 {
+            // Emit file prologue
+            file.write_all(
+                b"
+pub struct Registers {
+    Zero: u64,
+    Ra: u64,
+    Sp: u64,
+    Gp: u64,
+    Tp: u64,
+    T0: u64,
+    T1: u64,
+    T2: u64,
+    S0: u64,
+    S1: u64,
+    A0: u64,
+    A1: u64,
+    A2: u64,
+    A3: u64,
+    A4: u64,
+    A5: u64,
+    A6: u64,
+    A7: u64,
+    S2: u64,
+    S3: u64,
+    S4: u64,
+    S5: u64,
+    S6: u64,
+    S7: u64,
+    S8: u64,
+    S9: u64,
+    S10: u64,
+    S11: u64,
+    T3: u64,
+    T4: u64,
+    T5: u64,
+    T6: u64,
+    Pc: u64,
+}
+
+",
+            );
+        }
+
+        // Emit function prologue
+        file.write_all(
+            format!(
+                "
+#[no_mangle]
+pub extern fn inst_{:x}(regs: &mut Registers) {{
+",
+                pc
+            )
+            .as_bytes(),
+        );
+
+        let mut next_pc = pc.wrapping_add(4);
+
+        // Extract the opcode from the instruction
+        let opcode = inst & 0b1111111;
+
+        match opcode {
+            0b0110111 => {
+                // LUI
+                let inst = Utype::from(inst);
+                file.write_all(
+                    format!("regs.{} = {};\n", inst.rd, inst.imm as i64 as u64).as_bytes(),
+                );
+            }
+            0b0010111 => {
+                // AUIPC
+                let inst = Utype::from(inst);
+                file.write_all(
+                    format!(
+                        "regs.{} = regs.Pc.wrapping_add({});\n",
+                        inst.rd, inst.imm as i64 as u64
+                    )
+                    .as_bytes(),
+                );
+            }
+            0b1101111 => {
+                // JAL
+                let inst = Jtype::from(inst);
+                file.write_all(format!("regs.{} = regs.Pc.wrapping_add(4);\n", inst.rd).as_bytes());
+                next_pc = pc.wrapping_add(inst.imm as i64 as u64);
+            }
+            0b1100111 => {
+                // We know it's an Itype
+                let inst = Itype::from(inst);
+
+                match inst.funct3 {
+                    0b000 => {
+                        // JALR
+                        file.write_all(
+                            format!("regs.{} = regs.Pc.wrapping_add(4);\n", inst.rd).as_bytes(),
+                        );
+                        let target = self.reg(inst.rs1).wrapping_add(inst.imm as i64 as u64);
+                        next_pc = target;
+                    }
+                    _ => unimplemented!("Unexpected {:7b}", opcode),
+                }
+            }
+            0b1100011 => {
+                // We know it's an Btype
+                let inst = Btype::from(inst);
+                let rs1 = self.reg(inst.rs1);
+                let rs2 = self.reg(inst.rs2);
+
+                match inst.funct3 {
+                    0b000 => {
+                        // BEQ
+                        if rs1 == rs2 {
+                            next_pc = pc.wrapping_add(inst.imm as i64 as u64);
+                        }
+                    }
+                    0b001 => {
+                        // BNE
+                        if rs1 != rs2 {
+                            next_pc = pc.wrapping_add(inst.imm as i64 as u64);
+                        }
+                    }
+                    0b100 => {
+                        // BLT
+                        if (rs1 as i64) < (rs2 as i64) {
+                            next_pc = pc.wrapping_add(inst.imm as i64 as u64);
+                        }
+                    }
+                    0b101 => {
+                        // BGE
+                        if (rs1 as i64) >= (rs2 as i64) {
+                            next_pc = pc.wrapping_add(inst.imm as i64 as u64);
+                        }
+                    }
+                    0b110 => {
+                        // BLTU
+                        if (rs1 as u64) < (rs2 as u64) {
+                            next_pc = pc.wrapping_add(inst.imm as i64 as u64);
+                        }
+                    }
+                    0b111 => {
+                        // BGEU
+                        if (rs1 as u64) >= (rs2 as u64) {
+                            next_pc = pc.wrapping_add(inst.imm as i64 as u64);
+                        }
+                    }
+                    _ => unimplemented!("Unexpected {:7b}", opcode),
+                }
+            }
+            0b0000011 => {
+                // We know it's an Itype
+                let inst = Itype::from(inst);
+
+                // Compute the address
+                let addr =
+                    VirtAddr(self.reg(inst.rs1).wrapping_add(inst.imm as i64 as u64) as usize);
+
+                match inst.funct3 {
+                    0b000 => {
+                        // LB
+                        let mut tmp = [0u8; 1];
+                        self.memory.read_into(addr, &mut tmp);
+                        self.set_reg(inst.rd, i8::from_le_bytes(tmp) as i64 as u64);
+                    }
+                    0b001 => {
+                        // LH
+                        let mut tmp = [0u8; 2];
+                        self.memory.read_into(addr, &mut tmp);
+                        self.set_reg(inst.rd, i16::from_le_bytes(tmp) as i64 as u64);
+                    }
+                    0b010 => {
+                        // LW
+                        let mut tmp = [0u8; 4];
+                        self.memory.read_into(addr, &mut tmp);
+                        self.set_reg(inst.rd, i32::from_le_bytes(tmp) as i64 as u64);
+                    }
+                    0b011 => {
+                        // LD
+                        let mut tmp = [0u8; 8];
+                        self.memory.read_into(addr, &mut tmp);
+                        self.set_reg(inst.rd, i64::from_le_bytes(tmp) as u64);
+                    }
+                    0b100 => {
+                        // LBU
+                        let mut tmp = [0u8; 1];
+                        self.memory.read_into(addr, &mut tmp);
+                        self.set_reg(inst.rd, u8::from_le_bytes(tmp) as u64);
+                    }
+                    0b101 => {
+                        // LHU
+                        let mut tmp = [0u8; 2];
+                        self.memory.read_into(addr, &mut tmp);
+                        self.set_reg(inst.rd, u16::from_le_bytes(tmp) as u64);
+                    }
+                    0b110 => {
+                        // LWU
+                        let mut tmp = [0u8; 4];
+                        self.memory.read_into(addr, &mut tmp);
+                        self.set_reg(inst.rd, u32::from_le_bytes(tmp) as u64);
+                    }
+                    _ => unimplemented!("Unexpected {:7b}", opcode),
+                }
+            }
+            0b0100011 => {
+                // We know it's an Stype
+                let inst = Stype::from(inst);
+
+                // Compute the address
+                let addr =
+                    VirtAddr(self.reg(inst.rs1).wrapping_add(inst.imm as i64 as u64) as usize);
+
+                match inst.funct3 {
+                    0b000 => {
+                        // SB
+                        let val = self.reg(inst.rs2) as u8;
+                        self.memory.write(addr, val);
+                    }
+                    0b001 => {
+                        // SH
+                        let val = self.reg(inst.rs2) as u16;
+                        self.memory.write(addr, val);
+                    }
+                    0b010 => {
+                        // SW
+                        let val = self.reg(inst.rs2) as u32;
+                        self.memory.write(addr, val);
+                    }
+                    0b011 => {
+                        // SD
+                        let val = self.reg(inst.rs2) as u64;
+                        self.memory.write(addr, val);
+                    }
+                    _ => unimplemented!("Unexpected {:7b}", opcode),
+                }
+            }
+            0b0010011 => {
+                // We know it's an Itype
+                let inst = Itype::from(inst);
+
+                let rs1 = self.reg(inst.rs1);
+                let imm = inst.imm as i64 as u64;
+
+                match inst.funct3 {
+                    0b000 => {
+                        // ADDI
+                        self.set_reg(inst.rd, rs1.wrapping_add(imm));
+                    }
+                    0b001 => {
+                        // SLLI
+                        let shamt = inst.imm & 0b111111;
+                        self.set_reg(inst.rd, rs1 << shamt);
+                    }
+                    0b010 => {
+                        // SLTI
+                        if (rs1 as i64) < (imm as i64) {
+                            self.set_reg(inst.rd, 1);
+                        } else {
+                            self.set_reg(inst.rd, 0);
+                        }
+                    }
+                    0b011 => {
+                        // SLTUI
+                        if (rs1 as u64) < (imm as u64) {
+                            self.set_reg(inst.rd, 1);
+                        } else {
+                            self.set_reg(inst.rd, 0);
+                        }
+                    }
+                    0b100 => {
+                        // XORI
+                        self.set_reg(inst.rd, rs1 ^ imm);
+                    }
+                    0b101 => {
+                        let mode = (inst.imm >> 6) & 0b111111;
+                        match mode {
+                            0b000000 => {
+                                // SRLI
+                                let shamt = inst.imm & 0b111111;
+                                self.set_reg(inst.rd, rs1 >> shamt);
+                            }
+                            0b010000 => {
+                                // SRAI
+                                let shamt = inst.imm & 0b111111;
+                                self.set_reg(inst.rd, ((rs1 as i64) >> shamt) as u64);
+                            }
+                            _ => unimplemented!("Unexpected {:7b}", opcode),
+                        }
+                    }
+                    0b110 => {
+                        // ORI
+                        self.set_reg(inst.rd, rs1 | imm);
+                    }
+                    0b111 => {
+                        // ANDI
+                        self.set_reg(inst.rd, rs1 & imm);
+                    }
+                    _ => unimplemented!("Unexpected {:7b}", opcode),
+                }
+            }
+            0b0110011 => {
+                // We know it's an Rtype
+                let inst = Rtype::from(inst);
+
+                let rs1 = self.reg(inst.rs1);
+                let rs2 = self.reg(inst.rs2);
+
+                match (inst.funct7, inst.funct3) {
+                    (0b0000000, 0b000) => {
+                        // ADD
+                        self.set_reg(inst.rd, rs1.wrapping_add(rs2));
+                    }
+                    (0b0100000, 0b000) => {
+                        // SUB
+                        self.set_reg(inst.rd, rs1.wrapping_sub(rs2));
+                    }
+                    (0b0000000, 0b001) => {
+                        // SLL
+                        let shamt = rs2 & 0b111111;
+                        self.set_reg(inst.rd, rs1 << shamt);
+                    }
+                    (0b0000000, 0b010) => {
+                        // SLT
+                        if (rs1 as i64) < (rs2 as i64) {
+                            self.set_reg(inst.rd, 1);
+                        } else {
+                            self.set_reg(inst.rd, 0);
+                        }
+                    }
+                    (0b0000000, 0b011) => {
+                        // SLTU
+                        if (rs1 as u64) < (rs2 as u64) {
+                            self.set_reg(inst.rd, 1);
+                        } else {
+                            self.set_reg(inst.rd, 0);
+                        }
+                    }
+                    (0b0000000, 0b100) => {
+                        // XOR
+                        self.set_reg(inst.rd, rs1 ^ rs2);
+                    }
+                    (0b0000000, 0b101) => {
+                        // SRL
+                        let shamt = rs2 & 0b111111;
+                        self.set_reg(inst.rd, rs1 >> shamt);
+                    }
+                    (0b0100000, 0b101) => {
+                        // SRA
+                        let shamt = rs2 & 0b111111;
+                        self.set_reg(inst.rd, ((rs1 as i64) >> shamt) as u64);
+                    }
+                    (0b0000000, 0b110) => {
+                        // OR
+                        self.set_reg(inst.rd, rs1 | rs2);
+                    }
+                    (0b0000000, 0b111) => {
+                        // AND
+                        self.set_reg(inst.rd, rs1 & rs2);
+                    }
+                    _ => unreachable!(),
+                }
+            }
+            0b0001111 => {
+                let inst = Itype::from(inst);
+
+                match inst.funct3 {
+                    0b000 => {
+                        // FENCE
+                        unimplemented!("Unhandled FENCE instruction at {:#x}", pc);
+                    }
+                    _ => unreachable!(),
+                }
+            }
+            0b1110011 => match inst {
+                0b000000000000001110011 => {
+                    // ECALL
+                    panic!("JIT ECALL"); //return Err(EmuStop::Syscall);
+                }
+                0b100000000000001110011 => {
+                    // EBREAK
+                    unimplemented!("Unhandled EBREAK instruction at {:#x}", pc);
+                }
+                _ => unreachable!(),
+            },
+            0b0011011 => {
+                // We know it's an Itype
+                let inst = Itype::from(inst);
+
+                let rs1 = self.reg(inst.rs1) as u32;
+                let imm = inst.imm as u32;
+
+                match inst.funct3 {
+                    0b000 => {
+                        // ADDI
+                        self.set_reg(inst.rd, rs1.wrapping_add(imm) as i32 as i64 as u64);
+                    }
+                    0b001 => {
+                        let mode = (inst.imm >> 5) & 0b1111111;
+                        match mode {
+                            0b000000 => {
+                                // SLLIW
+                                let shamt = inst.imm & 0b11111;
+                                self.set_reg(inst.rd, (rs1 << shamt) as i32 as i64 as u64);
+                            }
+                            _ => unimplemented!("Unexpected {:7b}", opcode),
+                        }
+                    }
+                    0b101 => {
+                        let shamt = inst.imm & 0b11111;
+                        let mode = (inst.imm >> 5) & 0b1111111;
+                        match mode {
+                            0b0000000 => {
+                                // SRLIW
+                                self.set_reg(inst.rd, (rs1 >> shamt) as i32 as i64 as u64);
+                            }
+                            0b0100000 => {
+                                // SRAIW
+                                self.set_reg(inst.rd, ((rs1 as i32) >> shamt) as i64 as u64);
+                            }
+                            _ => unimplemented!("Unexpected {:7b}", opcode),
+                        }
+                    }
+                    _ => unimplemented!("Unexpected {:7b}", opcode),
+                }
+            }
+            0b0111011 => {
+                // We know it's an Rtype
+                let inst = Rtype::from(inst);
+
+                let rs1 = self.reg(inst.rs1) as u32;
+                let rs2 = self.reg(inst.rs2) as u32;
+
+                match (inst.funct7, inst.funct3) {
+                    (0b0000000, 0b000) => {
+                        // ADDW
+                        self.set_reg(inst.rd, rs1.wrapping_add(rs2) as i32 as i64 as u64);
+                    }
+                    (0b0100000, 0b000) => {
+                        // SUBW
+                        self.set_reg(inst.rd, rs1.wrapping_sub(rs2) as i32 as i64 as u64);
+                    }
+                    (0b0000000, 0b001) => {
+                        // SLLW
+                        let shamt = rs2 & 0b111111;
+                        self.set_reg(inst.rd, (rs1 << shamt) as i32 as i64 as u64);
+                    }
+                    (0b0000000, 0b101) => {
+                        // SRLW
+                        let shamt = rs2 & 0b111111;
+                        self.set_reg(inst.rd, (rs1 >> shamt) as i32 as i64 as u64);
+                    }
+                    (0b0100000, 0b101) => {
+                        // SRAW
+                        let shamt = rs2 & 0b111111;
+                        self.set_reg(inst.rd, ((rs1 as i32) >> shamt) as i64 as u64);
+                    }
+                    _ => unreachable!(),
+                }
+            }
+            _ => unimplemented!("Unhandled opcode {:#09b}\n", opcode),
+        }
+
+        // Output function epilogue
+        file.write_all(format!("unsafe {{ inst_{:x}(regs); }}\n}}", next_pc).as_bytes());
+
+        self.jit_insts += 1;
+        if self.jit_insts >= JIT_INSTS_MAX {
+            // Output file epilogue
+            file.flush();
+            self.jit_file.replace(None);
+        }
+    }
 }
 
 #[derive(Debug)]
@@ -898,6 +1397,12 @@ impl From<u32> for Register {
     fn from(val: u32) -> Self {
         assert!(val < 32);
         unsafe { core::ptr::read_unaligned(&(val as usize) as *const usize as *const Register) }
+    }
+}
+
+impl fmt::Display for Register {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        fmt::Debug::fmt(self, f)
     }
 }
 
